69ee9bf978ea101298a42ab8fb7b26e52fd33ae3
Lemuria <74448738+a-random-lemurian@users.noreply.github.com>
2022-01-01 21:38:17 +0800

    Automated formatting by Black

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ def main(infile, outfile):
             rawJSON TEXT,
             numID INTEGER PRIMARY KEY AUTOINCREMENT
         );
-        """)
+        """
+    )
 
     f = open(infile, "r")
     objects = ijson.items(f, "messages.item")
@@ -38,10 +39,10 @@ def main(infile, outfile):
     print("Lemuria / dcejson -> sqlite")
     print("Conversion in progress.....")
     for i, itm in enumerate(obj):
-        print(f"Processed message {i}                              ",end='\r')
+        print(f"Processed message {i}                              ", end="\r")
         author = itm.get("author")
         cur.execute(
-         """
+            """
          INSERT INTO messages (
             id,
             type,
@@ -65,31 +66,38 @@ def main(infile, outfile):
              ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?
          )
          """,
-         (itm.get('id'),
-         itm.get('type'),
-         itm.get('timestamp'),
-         itm.get('timestampEdited'),
-         itm.get('callEndedTimestamp'),
-         itm.get('isPinned'),
-         itm.get('content'),
-
-         author.get('id'),
-         author.get('name'),
-         author.get('discriminator'),
-         author.get('nickname'),
-         author.get('isBot'),
-         author.get('avatarUrl'),
-         author.get('color'),
-         str(itm)
-         )
+            (
+                itm.get("id"),
+                itm.get("type"),
+                itm.get("timestamp"),
+                itm.get("timestampEdited"),
+                itm.get("callEndedTimestamp"),
+                itm.get("isPinned"),
+                itm.get("content"),
+                author.get("id"),
+                author.get("name"),
+                author.get("discriminator"),
+                author.get("nickname"),
+                author.get("isBot"),
+                author.get("avatarUrl"),
+                author.get("color"),
+                str(itm),
+            ),
         )
     print(f"Processed {i} messages.")
     db.commit()
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Convert DCE .json output files into SQLite databases.')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Convert DCE .json output files into SQLite databases."
+    )
     parser.add_argument("input", help="Path to input file.")
-    parser.add_argument("-o", "--output", help="Path to output file", default="ExportedDiscordMessages.db")
+    parser.add_argument(
+        "-o",
+        "--output",
+        help="Path to output file",
+        default="ExportedDiscordMessages.db",
+    )
     args = parser.parse_args()
     main(args.input, args.output)
